17b0932c63292d7b1fe4425d969370cd8c3d9923
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-10-22 19:26:12 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chloropleth::Application.config.secret_key_base = '0f556c6e62ea583e6e224f588e46fd9e40937597f00061537f067f2ac6f45317989fc9bc4d84b393e5783ca91f3a783366206ea9bf078bd9c5dcf25ec56c50f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chloropleth::Application.config.session_store :cookie_store, key: '_chloropleth_session'
